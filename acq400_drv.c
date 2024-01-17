4f30e902f3d87db516bbd3ec7ffa6fdc1b5a3d1a
Peter Milne <peter.milne@d-tacq.com>
2024-01-17 12:28:39 +0000

    knobs for site 0 rtm12, acq480 sites: map4 map8 	modified:   init/acq420.init

--- acq400_drv.c
+++ acq400_drv.c
@@ -23,7 +23,7 @@
 #include "dmaengine.h"
 
 
-#define REVID 			"3.830"
+#define REVID 			"3.831"
 #define MODULE_NAME             "acq420"
 
 /* Define debugging for use during our driver bringup */
