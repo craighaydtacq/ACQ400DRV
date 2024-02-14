13385d271d22305c1fe9bd5ae9a0e1dc6846e329
Peter Milne <peter.milne@d-tacq.com>
2024-02-14 09:35:21 +0000

    hex_char_to_bin.h : for creating arbitrary bitsets  On branch main2 	new file:   hex_char_to_bin.cpp 	new file:   hex_char_to_bin.h

---
+++ hex_char_to_bin.cpp
@@ -0,0 +1,44 @@
+/*
+ * hex_char_to_bin.cpp
+ *
+ *  Created on: 9 Feb 2024
+ *      Author: pgm
+ */
+
+#include <string>
+
+#include <ctype.h>
+
+static std::string hexCharToBin( char c ) {
+    switch( toupper(c) ) {
+        case '0'    : return "0000";
+        case '1'    : return "0001";
+        case '2'    : return "0010";
+        case '3'    : return "0011";
+        case '4'    : return "0100";
+        case '5'    : return "0101";
+        case '6'    : return "0110";
+        case '7'    : return "0111";
+        case '8'    : return "1000";
+        case '9'    : return "1001";
+        case 'A'    : return "1010";
+        case 'B'    : return "1011";
+        case 'C'    : return "1100";
+        case 'D'    : return "1101";
+        case 'E'    : return "1110";
+        case 'F'    : return "1111";
+    }
+    return "ERROR";
+}
+
+
+std::string hexStrToBin( const std::string & hs ) {
+    std::string bin;
+    for( auto c : hs ) {
+        bin += hexCharToBin( c );
+    }
+    return bin;
+}
+
+
+
---
+++ hex_char_to_bin.h
@@ -0,0 +1,15 @@
+/*
+ * hex_char_to_bin.h
+ *
+ *  Created on: 9 Feb 2024
+ *      Author: pgm
+ */
+
+#ifndef HEX_CHAR_TO_BIN_H_
+#define HEX_CHAR_TO_BIN_H_
+
+
+std::string hexStrToBin( const std::string & hs );
+
+
+#endif /* HEX_CHAR_TO_BIN_H_ */
