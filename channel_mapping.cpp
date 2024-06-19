93a4ebbbddc23eaa97e4a0055ac226d223a165dc
Peter Milne <peter.milne@d-tacq.com>
2024-06-19 17:16:53 +0100

    use has_axi_dma_stack to detect STACK mapping  On branch main2 	modified:   channel_mapping.cpp

--- channel_mapping.cpp
+++ channel_mapping.cpp
@@ -14,13 +14,14 @@
 namespace G {
 	unsigned int nchan;
 	unsigned int naxi_dma;
-	bool use_mgtdram;
+	char use_mgtdram;
+	char stack;
 	char model[32];
 };
 
 int print_the_map(void)
 {
-	if (G::use_mgtdram && strncmp(G::model, "ACQ48", 5) == 0 && G::nchan > 8){
+	if ((G::stack || G::use_mgtdram) && strncmp(G::model, "ACQ48", 5) == 0 && G::nchan > 8){
 		int blocks[] = { 0, 8, 4, 12, 16, 24, 20, 28, 32, 40, 36, 44 };
 		for (unsigned int ic = 0; ic < G::nchan; ++ic){
 			printf("%d%c", blocks[ic/4]+ic%4, ic+1==G::nchan? '\n': ',');
@@ -42,5 +43,6 @@ int main(int argc, char** argv) {
 	getKnob(0, "/etc/acq400/1/MODEL", G::model);
 	getKnob(0, "/etc/acq400/0/NCHAN", &G::nchan);
 	getKnob(0, "/dev/acq400.0.knobs/has_axi_dma", &G::naxi_dma);
+	getKnob(0, "/dev/acq400.0.knobs/has_axi_dma_stack", &G::stack);
 	return print_the_map();
 }
