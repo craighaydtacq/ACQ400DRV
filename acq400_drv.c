4998e736bd64666c9d62171f25a089be49ac36e6
Peter Milne <peter.milne@d-tacq.com>
2024-08-11 15:14:09 +0100

    3855 .. AO422 may have rgm as well..  On branch main2 	modified:   acq400.h 	modified:   acq400_drv.c

--- acq400_drv.c
+++ acq400_drv.c
@@ -23,7 +23,7 @@
 #include "dmaengine.h"
 
 
-#define REVID 			"3.854"
+#define REVID 			"3.855"
 #define MODULE_NAME             "acq420"
 
 /* Define debugging for use during our driver bringup */
