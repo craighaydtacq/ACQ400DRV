188edc020a68ddf5c529d7b554f425e4f0f68c2b
Peter Milne <peter.milne@d-tacq.com>
2025-01-30 11:00:48 +0000

    site 0 create afwm links, if they exist.
    NB: the simple ln option results in dud links if the target
    eg afwm
    doesn't exist
    
     On branch main2
    	modified:   acq400_drv.c
    	modified:   init/acq420.init

--- acq400_drv.c
+++ acq400_drv.c
@@ -23,7 +23,7 @@
 #include "dmaengine.h"
 
 
-#define REVID 			"3.900"
+#define REVID 			"3.901"
 #define MODULE_NAME             "acq420"
 
 /* Define debugging for use during our driver bringup */
