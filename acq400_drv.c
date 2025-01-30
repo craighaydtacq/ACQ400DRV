86487448bdb6ecd426d2e905cc906f3853a9f63f
Peter Milne <peter.milne@d-tacq.com>
2025-01-30 11:57:11 +0000

    R3.902 show AGG_FIFO_WMASK debug reg where appropriate

--- acq400_drv.c
+++ acq400_drv.c
@@ -23,7 +23,7 @@
 #include "dmaengine.h"
 
 
-#define REVID 			"3.901"
+#define REVID 			"3.902"
 #define MODULE_NAME             "acq420"
 
 /* Define debugging for use during our driver bringup */
